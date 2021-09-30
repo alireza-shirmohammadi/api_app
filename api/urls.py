1f238128704f1eedca07720a6249cbbe0c2588a2
Alireza <alireza.sh076@gmail.com>
2021-09-30 14:49:48 +0330

    add token authentication

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('api/user/actknowlage',views.user_exist_actknowlage),
     path('api/user/create',views.create_user),
     path('api/user/login',views.user_login),
+    path('api/user/test',views.test),
 ]
--- api/views.py
+++ api/views.py
@@ -1,16 +1,20 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
 from .models import UserProfile
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import serializers
 from .serializers import User_profileSerializer
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+
+
 @api_view(['GET'])
 def user_exist_actknowlage(request):
     email=request.data['email']
+    device = request.data['device']
     actknwolage=UserProfile.objects.filter(email=email).exists()
     print(actknwolage)
     return Response(data=actknwolage, status=status.HTTP_200_OK)
@@ -23,9 +27,10 @@ def create_user(request):
     phone=request.data['phone']
     name=request.data['name']
     last_name=request.data['last_name']
+    device = request.data['device']
     if not User.objects.filter(username=user_name).exists():
         user=User.objects.create_user(username=user_name,password=password)
-        user_profile=UserProfile.objects.create(user_name=user,phone=phone,name=name,last_name=last_name,
+        UserProfile.objects.create(user_name=user,phone=phone,name=name,last_name=last_name,
                                                 email=email)
         return Response(data='success', status=status.HTTP_200_OK)
     else:
@@ -33,10 +38,19 @@ def create_user(request):
 
 @api_view(['GET'])
 def user_login(request):
-    user_name=request.data['user_name']
+    user_name=request.data['username']
     password = request.data['password']
+    device = request.data['device']
     user=authenticate(username=user_name, password=password)
     if user :
-        return Response(data='successfully logged in', status=status.HTTP_200_OK)
+        token , created =Token.objects.get_or_create(user=user)
+        return Response(data={'token' : token.key}, status=status.HTTP_200_OK)
     else:
         return Response(data='username or password invalid', status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test(request):
+    content='Token is working'
+    return Response(content)
